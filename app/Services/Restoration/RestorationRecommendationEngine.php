144d48c60a61f203b0a463bdcbadf84ab26bd6fc
jule <rafafazli7@gmail.com>
2026-09-03 19:05:01 +0700

    chore: scaffold Drone Land Analysis classes (empty stubs, not yet implemented)

---
+++ app/Services/Restoration/RestorationRecommendationEngine.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Restoration;
+
+class RestorationRecommendationEngine
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
